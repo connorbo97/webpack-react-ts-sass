aa05ca2b522a2786bf011fbbd2139d1f27148ad4
Connor B <connorbo97@gmail.com>
2021-07-11 19:27:12 -0700

    update dev server to use differetn webpack config key

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -6,11 +6,13 @@ const commonConfig = require("./common");
 module.exports = merge(commonConfig, {
   mode: "development",
   entry: [
-    "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
+    // Errors should be considered fatal in development
+    require.resolve('react-error-overlay'),
     "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
     "./index.tsx", // the entry point of our app
   ],
   devServer: {
+    port: 8080,
     hot: true, // enable HMR on the server
     historyApiFallback: true, // fixes error 404-ish errors when using react router :see this SO question: https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url 
   },
